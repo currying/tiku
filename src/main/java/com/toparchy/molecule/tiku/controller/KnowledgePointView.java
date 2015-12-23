2548646673d71a34ce56adb9db9929f45bf9f368
王宇轩 <currying@gmail.com>
2015-12-23 15:31:59 +0800

    Signed-off-by: 王宇轩 <currying@gmail.com>

---
+++ src/main/java/com/toparchy/molecule/tiku/controller/KnowledgePointView.java
@@ -0,0 +1,17 @@
+package com.toparchy.molecule.tiku.controller;
+
+import java.io.Serializable;
+
+import javax.enterprise.inject.Model;
+import javax.faces.view.ViewScoped;
+
+@Model
+@ViewScoped
+public class KnowledgePointView implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6693847761860132390L;
+
+}
